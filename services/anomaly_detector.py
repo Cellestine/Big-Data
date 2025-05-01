15a297ed36833ab4e38d9eb902532f1f8c27c3a7
Cellestine <celestine_fofana@outlook.com>
2025-05-01 16:06:36 +0200

    [evol] fonction is_anomalous

--- services/anomaly_detector.py
+++ services/anomaly_detector.py
@@ -1,7 +1,4 @@
 class AnomalyDetector:
-    def __init__(self, threshold=10000):
-        self.threshold = threshold
-
     def is_anomalous(self, transaction):
-        amount = transaction.get("amount", 0)
-        return amount > self.threshold
+        return transaction.get("FLAG", 0) != 0
+        
